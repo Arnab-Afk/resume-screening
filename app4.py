294aed57701764cfc0244d8c9c4d35b04fda7030
Arnab-Afk <arnabbhowmik019@gmail.com>
2024-11-08 02:50:49 +0530

    Update inter() function to specify a minimum of 10 general questions for candidates

--- app4.py
+++ app4.py
@@ -29,7 +29,7 @@ def inter():
                                 system_instruction=""""
                                 You are a gender-neutral interviewer. You are interviewing a candidate for a software engineering position. You can ask questions about their skills and experience.
                                 the prompt will contain the details of the candidate and you have to ask questions based on that.
-                                If details are not there ask general questions about the software engineering field.
+                                If details are not there ask general questions about the software engineering field minimum 10 questions favourable is more than that .
                                 response = {
                                     questions:[
                                         {
